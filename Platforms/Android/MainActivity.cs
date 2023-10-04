1a7b2553dcf40aae581e9ba1e4a15357904bea91
FernandoHazel <fernandohazel1@gmail.com>
2023-10-04 10:52:24 -0500

    permissions added

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -7,5 +7,36 @@ namespace TikTalk
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            const int requestNotification = 0;
+            string[] notiPermission =
+            {
+                Manifest.Permission.PostNotifications
+            };
+
+            if ((int)Build.VERSION.SdkInt < 33)
+                return;
+
+            if (CheckSelfPermission(Manifest.Permission.PostNotifications) == Permission.Granted)
+                return;
+
+            RequestPermissions(notiPermission, requestNotification);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                var channel = new NotificationChannel("my_channel_id", "My Channel", NotificationImportance.Default)
+                {
+                    Description = "Channel description"
+                };
+
+                var notificationManager = GetSystemService(NotificationService) as NotificationManager;
+                notificationManager.CreateNotificationChannel(channel);
+            }
+
+
+        }
     }
 }
--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -1,6 +1,24 @@
 ﻿using Android.App;
 using Android.Runtime;
 
+// Needed for Picking photo/video
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage, MaxSdkVersion = 32)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaAudio)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaImages)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaVideo)]
+
+// Needed for Taking photo/video
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 32)]
+
+// Add these properties if you would like to filter out devices that do not have cameras, or set to false to make them optional
+[assembly: UsesFeature("android.hardware.camera", Required = true)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = false)]
+
+//Required so that the plugin can reschedule notifications upon a reboot
+[assembly: UsesPermission("android.permission.SCHEDULE_EXACT_ALARM")]
+[assembly: UsesPermission("android.permission.POST_NOTIFICATIONS")]
+
 namespace TikTalk
 {
     [Application]
